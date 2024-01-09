a2c491cbc5232021bc69611844e826fb2145d1cb
许木超 <xumuchaobj@kanyun.com>
2024-01-09 14:51:40 +0800

    del: heros

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,5 +1,4 @@
 import { Heading } from "./_components/heading";
-import { Heroes } from "./_components/heroes";
 import { Footer } from "./_components/footer";
 
 const MarketingPage = () => {
@@ -7,7 +6,6 @@ const MarketingPage = () => {
     <div className="min-h-full flex flex-col">
       <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10">
         <Heading />
-        <Heroes />
       </div>
       <Footer />
     </div>
