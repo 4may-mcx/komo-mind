8c0976b4a5003215578fe10779b83561b0a24816
许木超 <1522928438@qq.com>
2024-01-11 16:51:10 +0800

    feat: optimize sidebar layout

--- app/(main)/_components/sidebar/sidebar-container.tsx
+++ app/(main)/_components/sidebar/sidebar-container.tsx
@@ -6,23 +6,65 @@ import { useMediaQuery } from "usehooks-ts";
 
 const MIN_WIDTH = 50;
 const MAX_WIDTH = 240;
+const MIN_RESIZE_WIDTH = 80;
+const MAX_RESIZE_WIDTH = 180;
 
 export const SidebarContainer = ({ children }: { children: ReactNode }) => {
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   const isResizingRef = useRef(false);
+  const resizeStateRef = useRef({
+    width: MAX_WIDTH,
+    increasing: false,
+    decreasing: true,
+  });
   const sidebarRef = useRef<ElementRef<"aside">>(null);
   const [isResetting, setIsResetting] = useState(false);
+
+  const handleResizingPeriod = () => {
+    setIsResetting(true);
+    setTimeout(() => setIsResetting(false), 300);
+  };
+
+  const updateResizeState = (newWidth: number) => {
+    const { width } = resizeStateRef.current;
+    const increasing = newWidth > width;
+    const decreasing = newWidth < width;
+    resizeStateRef.current = {
+      width: newWidth,
+      increasing,
+      decreasing,
+    };
+  };
+
+  const adjustWidth = (newWidth: number) => {
+    const { increasing, decreasing } = resizeStateRef.current;
+
+    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
+    if (newWidth > MAX_WIDTH) newWidth = MAX_WIDTH;
+
+    if (newWidth < MAX_RESIZE_WIDTH && newWidth > MIN_RESIZE_WIDTH) {
+      isResizingRef.current = false;
+      if (decreasing) {
+        newWidth = MIN_WIDTH;
+      }
+      if (increasing) {
+        newWidth = MAX_WIDTH;
+      }
+      handleResizingPeriod();
+    }
+
+    return newWidth;
+  };
 
   const handleMouseMove = (e: MouseEvent) => {
     if (!isResizingRef.current) return;
     let newWidth = e.clientX;
 
-    if (newWidth < MIN_WIDTH) newWidth = MIN_WIDTH;
-    if (newWidth > MAX_WIDTH) newWidth = MAX_WIDTH;
+    updateResizeState(newWidth);
 
     if (sidebarRef.current) {
-      sidebarRef.current.style.width = `${newWidth}px`;
+      sidebarRef.current.style.width = `${adjustWidth(newWidth)}px`;
     }
   };
 
@@ -43,7 +85,7 @@ export const SidebarContainer = ({ children }: { children: ReactNode }) => {
 
   const resetWidth = () => {
     if (sidebarRef.current) {
-      setIsResetting(true);
+      handleResizingPeriod();
 
       const currentWidth = sidebarRef.current.style.width;
       if (currentWidth === MIN_WIDTH + "px") {
@@ -52,7 +94,6 @@ export const SidebarContainer = ({ children }: { children: ReactNode }) => {
         sidebarRef.current.style.width = isMobile ? "100%" : `${MIN_WIDTH}px`;
       }
     }
-    setTimeout(() => setIsResetting(false), 300);
   };
 
   return (
@@ -64,7 +105,7 @@ export const SidebarContainer = ({ children }: { children: ReactNode }) => {
         }}
         className={cn(
           "h-full overflow-y-auto relative flex flex-col",
-          isResetting && "transition-all ease-in-out duration-300",
+          isResetting && "transition-all ease-in-out duration-200",
           isMobile && "w-0"
         )}
       >
